63df2cc9dfb5fa3e8b86b92effeb431088adf978
Rodrigo Kreutz <rodrigo.kreutz@acad.pucrs.br>
2016-12-19 21:10:37 -0200

    Inclusão do gitignore, Cartfile para o carthage e renomeado a view controller.

---
+++ Tarefas/TasksViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Tarefas
+//
+//  Created by Rodrigo Kreutz on 19/12/16.
+//  Copyright © 2016 Rodrigo Kreutz. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+
+
+}
+
